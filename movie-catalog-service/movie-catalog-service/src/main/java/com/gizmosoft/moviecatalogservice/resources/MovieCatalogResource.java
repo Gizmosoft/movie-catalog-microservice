bd0edb4f30b24dc44e956980538422e94282ad49
Gizmosoft <Kartikey.hebbar@gmail.com>
2023-02-06 11:27:10 +0530

    First Commit

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/gizmosoft/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package com.gizmosoft.moviecatalogservice.resources;
+
+import com.gizmosoft.moviecatalogservice.model.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        return Collections.singletonList(
+                new CatalogItem("Titanic","Movie about sinking ships", 5)
+        );
+    }
+}
