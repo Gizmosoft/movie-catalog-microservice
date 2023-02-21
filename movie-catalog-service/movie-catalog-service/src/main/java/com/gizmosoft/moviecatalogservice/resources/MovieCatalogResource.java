aeaca3218a38935c82d41cdf78d0262a251de81c
Gizmosoft <Kartikey.hebbar@gmail.com>
2023-02-21 22:46:24 +0530

    merging with dev

--- movie-catalog-service/movie-catalog-service/src/main/java/com/gizmosoft/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/gizmosoft/moviecatalogservice/resources/MovieCatalogResource.java
@@ -15,6 +15,12 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
+        // get all rated movie ID
+
+        // For each movie ID, call movie info services and get details
+
+        // put them all together
+
         return Collections.singletonList(
                 new CatalogItem("Titanic","Movie about sinking ships", 5)
         );
